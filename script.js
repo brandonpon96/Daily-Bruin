c36f60530ee89c428e538f907a7396ec350d3c5d
zeehang <myque@live.com>
2016-01-30 21:30:11 -0800

    Merge branch 'master' into new-nav-bar
    # Conflicts:
    #	index.html
    #	script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-<<<<<<< HEAD:firstprojectsite/script.js
-  $('#logo').fadeIn(1000);
-  $('#logo').animate({
-=======
   $('#overlay').fadeIn(800);
   $('#overlay').animate({
->>>>>>> master:script.js
   	//opacity: 1.0,
   	top:'+=70'
   },800);
